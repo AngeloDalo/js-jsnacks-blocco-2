fa7321ca63327b6244b68553352cd56d57d0013b
AngeloDalo <angelo251158@gmail.com>
2021-11-30 09:33:05 +0100

    invitati falsi

--- jsnack-1/script.js
+++ jsnack-1/script.js
@@ -1,1 +1,27 @@
 /**Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.*/
+
+function randomNumeroNome(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function randomNumeroCognome(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const container = document.querySelector(".container");
+
+const nomiFalsi = ["Antonio", "Giuseppe", "Francesco", "Giovanni", "Angelo", "Maria", "Giulia", "Sara", "Rebecca", "Carlo"];
+
+const cognomiFalsi = ["Rossi", "Ferrari", "Russo", "Bianchi", "Esposito", "Bruno", "Greco", "Gallo", "Conti", "Marino"];
+
+let i=0;
+let invitatiFalsi = [];
+
+for (i=0; i<10; i++) {
+    let randomNome = randomNumeroNome (0, 9);
+    let randomCognome = randomNumeroCognome (0, 9);
+    invitatiFalsi[i] = i+1 +") " + nomiFalsi[randomNome] +  " " + cognomiFalsi[randomCognome] + " ";
+    container.innerHTML += invitatiFalsi[i];
+}
+
+//console.log(InvitatiFalsi);
